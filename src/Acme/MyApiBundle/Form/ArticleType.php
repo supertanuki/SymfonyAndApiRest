28864fc11eb0bd1abf42976f8c18c646d7d53f13
Richard <rhanna@itnetwork.fr>
2014-04-15 11:25:55 +0200

    form fort article entity

---
+++ src/Acme/MyApiBundle/Form/ArticleType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme\MyApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('author')
+            ->add('createdAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\MyApiBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acme_myapibundle_article';
+    }
+}
